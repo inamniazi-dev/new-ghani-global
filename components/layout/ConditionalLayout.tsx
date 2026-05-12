30e56988e64ad920b4f4d74255b53b8b5fa08599
inamniazi-dev <muhammadinamullahkhanniazi92@gmail.com>
2026-05-12 12:29:28 +0500

    New Conditional Layout For Translation Fix

--- components/layout/ConditionalLayout.tsx
+++ components/layout/ConditionalLayout.tsx
@@ -2,12 +2,10 @@
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import SocialSidebar from "@/components/layout/SocialSidebar";
-import GoogleTranslateProvider from "@/components/layout/GoogleTranslateProvider";
 
 export default function ConditionalLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <GoogleTranslateProvider />
       <Navbar />
       <SocialSidebar />
       <main>{children}</main>
